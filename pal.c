d1719076dfb2894a850d916dd1359fdff7b318b5
D.siva reddy <40803761+sleeva321@users.noreply.github.com>
2018-07-10 09:11:40 +0530

    Create pal.c

---
+++ pal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   int n, rev=0, rem,temp;
+   printf("Enter an integer: ");
+   scanf("%d", &n);
+
+  temp=n;
+   while(temp!=0)
+   {
+      rem=temp%10;
+      rev=rev*10+rem;
+      temp/=10;
+   } 
+
+  
+   if(rev==n) 
+      printf("yes");
+   else
+      printf("no");
+   return 0;
+}
